ee0c821f2c676c7d91c7d7d8c865fa8227cc405e
irfanharfiansyah <irfanharfiansyah37@gmail.com>
2022-06-27 15:37:41 +0700

    init Project

---
+++ src/app/inspection/show-inspection/show-inspection.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { InspectionApiService } from 'src/app/inspection-api.service';
+
+@Component({
+  selector: 'app-show-inspection',
+  templateUrl: './show-inspection.component.html',
+  styleUrls: ['./show-inspection.component.css'],
+})
+export class ShowInspectionComponent implements OnInit {
+  inspectionList$!: Observable<any[]>;
+  inspectionTypeList$!: Observable<any[]>;
+  inspectionTypeList: any = [];
+
+  //Map to display data associate with FK
+  inspectionTypeMap: Map<number, string> = new Map();
+  constructor(private service: InspectionApiService) {}
+
+  ngOnInit(): void {
+    this.inspectionList$ = this.service.getInspectionList();
+    this.inspectionTypeList$ = this.service.getInspectionTypeList();
+    this.refreshInspectionTypesMap();
+  }
+  refreshInspectionTypesMap() {
+    this.service.getInspectionTypeList().subscribe((data) => {
+      this.inspectionTypeList = data;
+      for (let i = 0; i < data.length; i++) {
+        this.inspectionTypeMap.set(
+          this.inspectionTypeList[i].id,
+          this.inspectionTypeList[i].inspectionName
+        );
+      }
+    });
+  }
+}
